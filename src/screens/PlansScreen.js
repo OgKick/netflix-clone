deb8ced1afd42f231f6f488b3015920a59a3e57d
Karin <ka_deutsch@hotmail.ch>
2021-02-07 10:33:11 +0100

    page dont break anymore

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,32 +1,82 @@
-import React from 'react'
-import Nav from '../Nav'
-import { useSelector } from "react-redux"
-import { selectUser } from "../features/userSlice"
-import './ProfileScreen.css'
-import { auth } from '../firebase'
-import PlansScreen from './PlansScreen'
-
-function ProfileScreen() {
+import React, { useEffect, useState } from "react";
+import { selectUser } from "../features/userSlice";
+import { useSelector } from "react-redux";
+import db from "../firebase";
+import "./PlansScreen.css";
+import { loadStripe } from '@stripe/stripe-js';
+function PlansScreen() {
+  const [products, setProducts] = useState([]);
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    db.collection("products")
+      .where("active", "==", true)
+      .get()
+      .then((querySnapshot) => {
+        const products = {};
+        querySnapshot.forEach(async (productDoc) => {
+          products[productDoc.id] = productDoc.data();
+          const priceSnap = await productDoc.ref.collection("prices").get();
+          priceSnap.docs.forEach((price) => {
+            products[productDoc.id].prices = {
+              priceId: price.id,
+              priceData: price.data(),
+            };
+          });
+        });
+        setProducts(products);
+      });
+  }, []);
+
+  const loadCheckout = async (priceId) => {
+    const docRef = await db
+      .collection("customers")
+      .doc(user.uid)
+      .collection("checkout_sessions")
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
+
+    docRef.snapshot(async (snap) => {
+      const { error, sessionId } = snap.data();
+      if (error) {
+        // Show error to your customers
+        // Check cloud function in the firebase console
+        alert(`An error occured, ${error.message}`);
+      }
+      if (sessionId) {
+        // we have a session, let redirect to checkout
+        // Init Stripe
+        const stripe = await loadStripe(
+          "pk_test_51IHj9qB06TjmJxpgxpWe0aSfU1HgZrKt0WICR5UPEJITHgdTg52VtoT4Qd5SlkRDts6hG0i1tOTFMAtiFfpEFLPk00qKeM1OIf"
+        );
+        stripe.redirectToCheckout({ sessionId });
+      }
+    });
+  };
   return (
-    <div className="profileScreen">
-      <Nav />
-      <div className="profileScreen__body">
-        <h1>Edit Profile</h1>
-        <div className="profileScreen__info">
-          <img src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg" alt="" />
-          <div className="profileScreen__details">
-            <h2>{user.email}</h2>
-            <div className="profileScreen__plans">
-              <h3>Plans</h3>
-              <PlansScreen />
-              <button onClick={() => auth.signOut()} className="profileScreen__signOut">Sign Out</button>
+    <div className="plansScreen">
+      {Object.entries(products).map(([productId, productData]) => {
+        // Todos Add some logic to check if the user's subscription is active
+        return (
+          <div className="plansScreen__plans">
+            <div className="plansScreen__info">
+              <h5>{productData.name}</h5>
+              <h6>{productData.description}</h6>
             </div>
+
+            <button
+              onClick={() => loadCheckout(productData.prices.priceId)}
+            >
+              Subscribe
+            </button>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default ProfileScreen
+export default PlansScreen;
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectUser } from '../features/userSlice'
-import { auth } from '../firebase'
-import Nav from '../Nav'
-import './ProfileScreen.css'
-import PlansScreen from './PlansScreen'
+import React from "react";
+import Nav from "../Nav";
+import { selectUser } from "../features/userSlice";
+import { useSelector } from "react-redux";
+import PlansScreen from './PlansScreen';
+import "./ProfileScreen.css";
+import { auth } from "../firebase";
 
 function ProfileScreen() {
-  const user = useSelector(selectUser)
-
+  const user = useSelector(selectUser);
   return (
     <div className="profileScreen">
       <Nav />
       <div className="profileScreen__body">
         <h1>Edit Profile</h1>
         <div className="profileScreen__info">
-          <img className="profileScreen__image" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="User Avatar" />
+          <img
+            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+            alt=""
+          />
           <div className="profileScreen__details">
             <h2>{user.email}</h2>
-
             <div className="profileScreen__plans">
               <h3>Plans</h3>
               <PlansScreen />
               <button
                 onClick={() => auth.signOut()}
-                className="profileScreen__signOut">Sign Out</button>
+                className="profileScreen__signout"
+              >
+                Sign Out
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ProfileScreen
+export default ProfileScreen;
